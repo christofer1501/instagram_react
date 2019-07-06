0a60adf1f8d87e4abccc9a14e727b82162e6fbdc
christofer1501 <christofer15@yandex.ru>
2019-07-06 22:11:00 +0300

    add prop-types into User component

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -3,6 +3,6 @@ import spinner from '../spinner.gif';
 
 const Spinner = () => (
   <img src={ spinner } alt="loading..." />
-)
+);
       
 export default Spinner;
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,12 +1,24 @@
-import React, {Component} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export default class User extends Component {
-  render() {
-    return (
-      <a href="#" className={this.props.min ? "user min" : "user"}>
-        <img  src={this.props.src} alt={this.props.alt}/>
-        <div>{this.props.name}</div>
-      </a>
-    )
-  }
-}
+const User = ({ src, alt, name, min }) => (
+  <a href="#" className={ min ? "user min" : "user" }>
+    <img  src={ src } alt={ alt } />
+    <div>{ name }</div>
+  </a>
+);
+
+export default User;
+
+User.propTypes = {
+  src: PropTypes.string,
+  alt: PropTypes.string,
+  name: PropTypes.string.isRequired,
+  min: PropTypes.bool,
+};
+
+User.defaultProps = {
+  src: 'https://kaarygar.com/images/profile/no-user.jpg',
+  alt: 'Изображение пользователя',
+  min: false,
+};
