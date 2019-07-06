c54fc77256247a7f900004bbff56d27700cf83b1
christofer1501 <christofer15@yandex.ru>
2019-07-06 21:43:37 +0300

    clean Pallete component

--- src/components/Pallete.jsx
+++ src/components/Pallete.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import ErrorMessage from './ErrorMessage';
 import Spinner from './Spinner';
 import InstaService from '../services/instaservice';
@@ -9,17 +9,13 @@ export default class Pallete extends Component {
     photos: [],
     error: false,
     loading: true,
-  }
+  };
 
   componentDidMount() {
-    this.updatePhotos();
-  }
-
-  updatePhotos() {
     this.InstaService.getAllPhotos()
     .then(this.onPhotosLoaded)
     .catch(this.onError)
-  }
+  };
 
   onPhotosLoaded = (photos) => {
     this.setState({
@@ -27,38 +23,36 @@ export default class Pallete extends Component {
       error: false,
       loading: false,
     })
-  }
+  };
 
   onError = (err) => {
     this.setState({
       error: true,
       loading: false,
     })
-  }
-
-  renderItems(arr) {
-    return arr.map((item) => {
-      const {src, alt, id} = item;
-      return (
-        <img src={src} alt={alt} key={id}></img>
-      )
-    })
-  }
+  };
 
   render() {
-    const {error, photos, loading} = this.state;
+    const { error, photos, loading } = this.state;
     if (error) {
       return <ErrorMessage/>
     } else if (!photos && loading) {
       return <Spinner/>
-    }
-
-    const items = this.renderItems(photos);
+    };
 
     return (
       <div className="palette">
-       {items}
+        {
+          photos.map(({ src, alt, id }) => (
+              <img 
+                src={ src }
+                alt={ alt }
+                key={ id }
+              />
+            )
+          )
+        }
       </div>
-    )
+    );
   }
-}
+};
