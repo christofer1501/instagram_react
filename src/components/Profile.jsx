30f835ea78d6701ee231c4a1e31e8ba492c4da5f
christofer1501 <christofer15@yandex.ru>
2019-07-06 22:04:06 +0300

    clear Profile component return

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,17 +2,15 @@ import React from 'react';
 import User from './User';
 import Pallete from './Pallete';
 
-const Profile = () => {
-  return (
-    <div className="container profile">
-      <User
-        src="https://pbs.twimg.com/profile_images/2654273615/26f23912924bb6ee9fb46d23b30f2509.jpeg"
-        alt="man"
-        name="some_coolman"
-      />
-      <Pallete/>
-    </div>
-  )
-}
+const Profile = () => (
+  <div className="container profile">
+    <User
+      src="https://pbs.twimg.com/profile_images/2654273615/26f23912924bb6ee9fb46d23b30f2509.jpeg"
+      alt="man"
+      name="some_coolman"
+    />
+    <Pallete/>
+  </div>
+);
 
 export default Profile;
