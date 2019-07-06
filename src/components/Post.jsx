605d8d7dfef10c45e091a293fefb46772625b858
christofer1501 <christofer15@yandex.ru>
2019-07-06 21:52:26 +0300

    clear Post component and add proptypes

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,22 +1,33 @@
-import React, {Component} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import User from './User';
 
-export default class Post extends Component {
-  render() {
-    return (
-      <div className="post">
-        <User 
-          src="http://buben.fm/img/artist/256/25486.png"
-          alt="man" 
-          name="ben_light" min/>
-        <img src={this.props.src} alt={this.props.alt}></img>
-        <div className="post__name">
-          some acc
-        </div>
-        <div className="post__descr">
-          some info about post
-        </div>
-      </div>
-    )
-  }
-}
+const Post = ({ src, alt }) => (
+  <div className="post">
+    <User 
+      src="http://buben.fm/img/artist/256/25486.png"
+      alt="man" 
+      name="ben_light" 
+      min
+    />
+    <img src={ src } alt={ alt } />
+    <div className="post__name">
+      some acc
+    </div>
+    <div className="post__descr">
+      some info about post
+    </div>
+  </div>
+);
+
+export default Post;
+
+Post.propTypes = {
+  src: PropTypes.string,
+  alt: PropTypes.string,
+};
+
+Post.defaultProps = {
+  src: 'https://www.automaxindiana.com/templates/10567/images/770x450.gif',
+  alt: 'Здесь должно быть фото поста',
+};
