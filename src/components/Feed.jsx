e5e42ef610470a74528fa01cd2a76a486eb959a1
christofer1501 <christofer15@yandex.ru>
2019-07-06 21:33:50 +0300

    clean Feed return

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,12 +1,12 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Posts from './Posts';
 import Users from './Users';
 
-export default function Feed() {
-  return(
-    <div className="container feed">
-      <Posts/>
-      <Users/>
-    </div>
-  )
-}
+const Feed = () => (
+  <div className="container feed">
+    <Posts/>
+    <Users/>
+  </div>
+)
+
+export default Feed;
